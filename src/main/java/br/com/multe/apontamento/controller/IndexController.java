2be005298f08098145e8062009386f5d24539bc1
Heitor Araujo <heitor.saraujo@gmail.com>
2015-04-23 14:36:47 -0300

    third commit

--- src/main/java/br/com/multe/apontamento/controller/IndexController.java
+++ src/main/java/br/com/multe/apontamento/controller/IndexController.java
@@ -1,11 +1,15 @@
 package br.com.multe.apontamento.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import br.com.multe.apontamento.model.Evento;
 import br.com.multe.apontamento.service.IEventoService;
 
 @Controller
@@ -23,7 +27,14 @@ public class IndexController
 	@RequestMapping(value = "/apontamento", method = RequestMethod.GET)
 	public @ResponseBody String allApontamentos()
 	{
-		eventoService.getAll();
-		return "";
+		String response = "";
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		List<Evento> eventos = eventoService.getAll();
+		for(Evento evento : eventos)
+		{			
+			response = sdf.format(evento.getInicio()) + "<br/>";
+		}
+		
+		return response;
 	}
 }
